7fec4ae7f46f450e032950c5a925acd4e10475a1
costar011 <dnaer456@gmail.com>
2023-02-13 00:02:18 +0900

    js

--- main.js
+++ main.js
@@ -37,3 +37,60 @@ const defaultOptions = {
     },
   },
 };
+
+var barOptions = {
+  ...defaultOptions,
+  chart: {
+    ...defaultOptions.chart,
+    type: "bar",
+  },
+  tooltip: {
+    enabled: true,
+    fillerSeriesColor: false,
+    style: { fontFamily: fontFamily },
+    y: {
+      formatter: (value) => {
+        return `${value}`;
+      },
+    },
+  },
+
+  series: [
+    {
+      name: "This Year",
+      data: [23, 55, 41, 37, 22, 43],
+    },
+    {
+      name: "Last Year",
+      data: [34, 32, 33, 52, 13, 54],
+    },
+  ],
+};
+
+colors: [colorPrimary, colorDefault];
+stroke: {
+  show: false;
+}
+
+grid: {
+  borderColor: "rgba(255, 255, 255, 0.05)";
+
+  padding: {
+    left: 0;
+    right: 0;
+    top: -16;
+    bottom: -8;
+  }
+}
+
+plotOptions: {
+  bar: {
+    horizontal: false;
+    columnWidth: "50%";
+    borderRadius: 6;
+  }
+}
+
+yaxis: {
+  show: false;
+}
