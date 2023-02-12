bd925f37a63966fcfb3d2dfadcd001bbdc164c17
costar011 <dnaer456@gmail.com>
2023-02-12 16:00:10 +0900

    chart js

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const colorPrimary = getComputedStyle(
   document.documentElement
-).getPropertyValue("");
+).getPropertyValue("--color-primary");
+trim();
 
-const colorDefault = getComputedStyle(document.documentElement);
+const colorDefault = getComputedStyle(
+  document.documentElement
+).getPropertyValue("--color-default");
+trim();
+
+const colorLabel = getComputedStyle(document.documentElement).getPropertyValue(
+  "--color-label"
+);
+trim();
+
+const fontFamily = getComputedStyle(document.documentElement).getPropertyValue(
+  "--font-family"
+);
+trim();
+
+const defaultOptions = {};
